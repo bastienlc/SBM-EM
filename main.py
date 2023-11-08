f39f89e85a941d4ada0ca8231cb11fb11ade8737
bastienlc <78959054+bastienlc@users.noreply.github.com>
2023-11-08 22:29:21 +0100

    Ajout de cohérence mais la log descend plus

--- main.py
+++ main.py
@@ -47,24 +47,17 @@ def e_step(X, alpha, pi):
     n = X.shape[0]
     Q = alpha.shape[0]
     tau = np.repeat(alpha.reshape(1, Q), n, axis=0)
-    precomputed_b_s = np.zeros((n, Q, n, Q))
-    for i in range(n):
-        for q in range(Q):
-            for j in range(n):
-                for l in range(Q):
-                    precomputed_b_s[i, q, j, l] = b(X[i, j], pi[q, l])
     for _ in range(MAX_FIXED_POINT_ITERATIONS):
         previous_tau = np.copy(tau)
         for i in range(n):
             for q in range(Q):
                 tau[i, q] = alpha[q]
                 for j in range(n):
-                    if j == i:
-                        continue
-                    tau[i, q] *= np.prod(
-                        np.float_power(precomputed_b_s[i, q, j, :], tau[j, :])
-                    )
-        # tau = tau / np.sum(tau, axis=1)[:, np.newaxis]
+                    if i != j:
+                        for l in range(Q):
+                            tau[i, q] *= b(X[i, j], pi[q, l]) ** previous_tau[j, l]
+            tau[i, :] /= np.sum(tau[i, :])
+
         if np.linalg.norm(previous_tau - tau, ord=1) < EPSILON:
             break
     return tau
@@ -78,12 +71,32 @@ def log_likelihood(X, alpha, pi, tau):
         for q in range(Q):
             ll += tau[i, q] * np.log(alpha[q]) - tau[i, q] * np.log(tau[i, q])
             for j in range(n):
-                if j == i:
-                    continue
-                else:
+                if i != j:
                     for l in range(Q):
                         ll += tau[i, q] * tau[j, l] * np.log(b(X[i, j], pi[q, l]))
     return ll
+
+
+def parameters_are_ok(alpha, pi, tau):
+    if np.abs(np.sum(alpha) - 1) > EPSILON:
+        return False
+    if np.any(alpha < 0):
+        return False
+    if np.any(alpha > 1):
+        return False
+    if np.any(pi < 0):
+        return False
+    if np.any(pi > 1):
+        return False
+    if np.any(tau < 0):
+        return False
+    if np.any(tau > 1):
+        return False
+    if np.any((np.sum(tau, axis=1) - 1) > EPSILON):
+        return False
+    if np.any(pi - pi.transpose() > EPSILON):
+        return False
+    return True
 
 
 def em_algorithm(X, Q):
@@ -97,8 +110,11 @@ def em_algorithm(X, Q):
         alpha, pi = m_step(X, tau)
         print(
             f"After EM iteration {i}/{EM_ITERATIONS} : Log likelihood {log_likelihood(X, alpha, pi, tau)}...",
-            end="\r",
+            end="",
         )
+        if not parameters_are_ok(alpha, pi, tau):
+            print(" Values warning.", end="")
+        print("\r", end="", flush=True)
     print()
     return alpha, pi, tau
 
@@ -123,8 +139,6 @@ def random_graph_from_parameters(n, Q, alpha, pi):
         Z[i, :] = np.random.multinomial(1, alpha)
     for i in range(n):
         for j in range(i + 1, n):
-            if i == j:
-                continue
             i_class = np.where(Z[i, :] == 1)[0][0]
             j_class = np.where(Z[j, :] == 1)[0][0]
             X[i, j] = np.random.binomial(1, pi[i_class, j_class])
@@ -149,15 +163,15 @@ def random_graph(n=None, Q=None, occupation=0.5):
 if __name__ == "__main__":
     # Test the algorithm on a random graph
     X, Z = random_graph_from_parameters(
-        20,
+        40,
         2,
         np.array([0.5, 0.5]),
-        np.array([[0.2, 0], [0, 0.2]]),
+        np.array([[0.2, 0.1], [0.1, 0.2]]),
     )
 
     draw_graph(X, Z)
 
-    alpha, pi, tau = em_algorithm(X, 3)
+    alpha, pi, tau = em_algorithm(X, 2)
     print(alpha)
     print(pi)
     print(tau)
