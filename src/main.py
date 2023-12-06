363795071278c26fd9f27f5e95bb7cda5c855ed0
bastienlc <78959054+bastienlc@users.noreply.github.com>
2023-12-06 21:34:45 +0100

    Rework main, add multiple EM inits

--- src/main.py
+++ src/main.py
@@ -2,49 +2,56 @@ import numpy as np
 import torch
 
 from constants import *
+from critertions import draw_critertion
 from graphs import draw_graph, random_graph
 from opti import e_step, init_tau, init_X, log_likelihood, m_step, parameters_are_ok
+from utils import drop_init, sort_parameters
 
 
-def em_algorithm(X, Q):
+def em_algorithm(X, Q, n_init=10):
+    if n_init >= EM_ITERATIONS:
+        raise ValueError(f"n_init should be smaller than {EM_ITERATIONS}")
     # Initialisation
     n = X.shape[0]
     X = init_X(X)
-    tau = init_tau(n, Q)
-    previous_ll = -1e100
+    tau = [init_tau(n, Q) for _ in range(n_init)]
+    previous_ll = [-1e100 for _ in range(n_init)]
     # EM algorithm
     for i in range(EM_ITERATIONS):
-        alpha, pi = m_step(X, tau)
-        tau = e_step(X, alpha, pi)
-        ll = log_likelihood(X, alpha, pi, tau)
+        for k in range(n_init):
+            alpha, pi = m_step(X, tau[k])
+            alpha, pi = sort_parameters(alpha, pi)
+            tau[k] = e_step(X, alpha, pi)
+            ll = log_likelihood(X, alpha, pi, tau[k]).item()
+
+            # Coherence checks
+            if not parameters_are_ok(alpha, pi, tau[k]):
+                raise ValueError("Parameters are not ok")
+            if previous_ll[k] - PRECISION > ll:
+                raise ValueError("Log likelihood is decreasing")
+
+            previous_ll[k] = ll
         print(
-            f"After EM iteration {i}/{EM_ITERATIONS} : Log likelihood {ll:5f}...",
+            f"After EM iteration {i}/{EM_ITERATIONS} : Mean log likelihood ({n_init} paths) {np.mean(previous_ll):5f}...",
             end="",
         )
-        if not parameters_are_ok(alpha, pi, tau):
-            print(" Values warning.", end="")
-            raise ValueError("Parameters are not ok")
-        if previous_ll - PRECISION > ll:
-            print(" Log likelihood warning.", end="")
-            raise ValueError("Log likelihood is decreasing")
-        print("Alpha", alpha, end="")
         print("\r", end="", flush=True)
-        if abs(ll - previous_ll) < PRECISION:
-            break
-        previous_ll = ll
+
+        # Drop some inits after some time
+        if i * n_init > EM_ITERATIONS // n_init:
+            n_init, tau, previous_ll = drop_init(n_init, tau, previous_ll)
     print()
 
-    if isinstance(alpha, torch.Tensor):
-        alpha = alpha.cpu().detach().numpy()
-        pi = pi.cpu().detach().numpy()
-        tau = tau.cpu().detach().numpy()
+    best_init = np.argmax(previous_ll)
+    best_tau = tau[best_init]
+    best_alpha, best_pi = m_step(X, best_tau)
 
-    return alpha, pi, tau
+    if isinstance(best_tau, torch.Tensor):
+        best_alpha = best_alpha.cpu().detach().numpy()
+        best_pi = best_pi.cpu().detach().numpy()
+        best_tau = best_tau.cpu().detach().numpy()
 
-
-def sort_parameters(alpha, pi):
-    sort_indices = np.argsort(alpha)
-    return alpha[sort_indices], pi[sort_indices, :][:, sort_indices]
+    return best_alpha, best_pi, best_tau
 
 
 if __name__ == "__main__":
@@ -55,11 +62,9 @@ if __name__ == "__main__":
     alpha, pi = sort_parameters(alpha, pi)
 
     draw_graph(X, Z)
+    draw_critertion(n)
 
-    estimated_alpha, estimated_pi, tau = em_algorithm(X, Q)
-    estimated_alpha, estimated_pi = sort_parameters(estimated_alpha, estimated_pi)
+    estimated_alpha, estimated_pi, tau = em_algorithm(X, Q, n_init=10)
 
-    print("Estimated alpha", estimated_alpha)
-    print("Alpha", alpha)
-    print("Estimated pi", estimated_pi)
-    print("Pi", pi)
+    print(estimated_alpha)
+    print(alpha)
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import torch
+
+from constants import *
+
+
+def sort_parameters(alpha, pi):
+    if isinstance(alpha, np.ndarray):
+        sort_indices = np.argsort(alpha)
+        return alpha[sort_indices], pi[sort_indices, :][:, sort_indices]
+    if isinstance(alpha, torch.Tensor):
+        sort_indices = torch.argsort(alpha)
+        return alpha[sort_indices], pi[sort_indices, :][:, sort_indices]
+
+
+def drop_init(n_init, tau_list, ll_list):
+    if n_init > 1:
+        worst_index = np.argmin(ll_list)
+        return (
+            n_init - 1,
+            tau_list[:worst_index] + tau_list[worst_index + 1 :],
+            np.delete(ll_list, worst_index),
+        )
+    else:
+        return n_init, tau_list, ll_list
