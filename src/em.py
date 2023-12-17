efac2514e21e2cbdee52945a428e79c29b13a7cb
bastienlc <78959054+bastienlc@users.noreply.github.com>
2023-12-17 23:24:39 +0100

    Return best value so far on KeyboardInterrupt

--- src/em.py
+++ src/em.py
@@ -54,50 +54,66 @@ def em_algorithm(
         pi: the estimated pi
         tau: the estimated tau
     """
-    # Initialisation
-    impl = get_implementation(implementation)
-    n = X.shape[0]
-    X = impl.input(X)
-    taus = [impl.init_tau(n, Q) for _ in range(n_init)]
-    log_likelihoods = np.full((n_init, iterations), -np.inf)
-    inits = list(range(n_init))
+    try:
+        # Initialisation
+        impl = get_implementation(implementation)
+        n = X.shape[0]
+        X = impl.input(X)
+        taus = [impl.init_tau(n, Q) for _ in range(n_init)]
+        log_likelihoods = np.full((n_init, iterations), -np.inf)
+        inits = list(range(n_init))
 
-    # EM algorithm
-    for i in range(iterations):
-        for init in inits:
-            inits_to_drop = []
+        best_tau, best_ll = None, -np.inf
 
-            taus[init], log_likelihoods[init, i] = em_iteration(impl, X, taus[init])
+        # EM algorithm
+        for i in range(iterations):
+            for init in inits:
+                inits_to_drop = []
 
-            if ENFORCE_INCREASING_LIKELIHOOD and (
-                log_likelihoods[init, i - 1] - PRECISION > log_likelihoods[init, i]
-            ):
-                inits_to_drop.append(init)
+                taus[init], log_likelihoods[init, i] = em_iteration(impl, X, taus[init])
 
-        inits = drop_inits(
-            inits,
-            i,
-            iterations,
-            log_likelihoods,
-            inits_to_drop=inits_to_drop,
-        )
+                if ENFORCE_INCREASING_LIKELIHOOD and (
+                    log_likelihoods[init, i - 1] - PRECISION > log_likelihoods[init, i]
+                ):
+                    inits_to_drop.append(init)
+
+                if log_likelihoods[init, i] > best_ll:
+                    best_tau, best_ll = taus[init], log_likelihoods[init, i]
+
+            inits = drop_inits(
+                inits,
+                i,
+                iterations,
+                log_likelihoods,
+                inits_to_drop=inits_to_drop,
+            )
+
+            if verbose:
+                print(
+                    f"EM iteration {i+1}/{iterations} | Max LL ({len(inits)} path{'s' if n_init>1 else ''}) {np.max(log_likelihoods[inits, i]):5f}",
+                    end="",
+                )
+                print("\r", end="")
 
         if verbose:
-            print(
-                f"EM iteration {i+1}/{iterations} | Max LL ({len(inits)} path{'s' if n_init>1 else ''}) {np.max(log_likelihoods[inits, i]):5f}",
-                end="",
-            )
-            print("\r", end="")
+            print("")
 
-    if verbose:
-        print("")
+        tau = taus[inits[0]]
+        alpha, pi = impl.m_step(X, tau)
 
-    tau = taus[inits[0]]
-    alpha, pi = impl.m_step(X, tau)
-
-    return (
-        impl.output(alpha),
-        impl.output(pi),
-        impl.output(tau),
-        log_likelihoods,
-    )
+        return (
+            impl.output(alpha),
+            impl.output(pi),
+            impl.output(tau),
+            log_likelihoods,
+        )
+    except KeyboardInterrupt:
+        if verbose:
+            print("")
+        alpha, pi = impl.m_step(X, best_tau)
+        return (
+            impl.output(alpha),
+            impl.output(pi),
+            impl.output(best_tau),
+            log_likelihoods,
+        )
