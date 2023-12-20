b0d6c0566127c30c062f25c67956629bf3912a1c
bastienlc <78959054+bastienlc@users.noreply.github.com>
2023-12-20 11:49:27 +0100

    Comment graphs.py

--- src/graphs.py
+++ src/graphs.py
@@ -1,9 +1,23 @@
+from typing import Optional, Tuple
+
 import networkx as nx
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-def draw_graph(X, Z, tight=False):
+def draw_graph(X: np.ndarray, Z: np.ndarray, tight: bool = False) -> None:
+    """
+    Draws a graph based on the adjacency matrix X and node assignments Z.
+
+    Parameters
+    ----------
+    X : np.ndarray
+        Adjacency matrix representing connections between nodes.
+    Z : np.ndarray
+        Node assignments matrix.
+    tight : bool, optional
+        Whether to use a tight layout for the graph visualization, by default False.
+    """
     n = X.shape[0]
     G = nx.Graph()
     for i in range(n):
@@ -17,21 +31,63 @@ def draw_graph(X, Z, tight=False):
     plt.show()
 
 
-def random_graph_from_parameters(n, Q, alpha, pi):
+def random_graph_from_parameters(
+    n: int, Q: int, alpha: np.ndarray, pi: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Generates a random SBM graph based on input parameters.
+
+    Parameters
+    ----------
+    n : int
+        Number of nodes in the graph.
+    Q : int
+        Number of classes.
+    alpha : np.ndarray
+        Class membership probabilities.
+    pi : np.ndarray
+        Connection probabilities between classes.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        A tuple containing the adjacency matrix X and the node assignments matrix Z.
+    """
     X = np.zeros((n, n))
     Z = np.zeros((n, Q))
     for i in range(n):
         Z[i, :] = np.random.multinomial(1, alpha)
+
     for i in range(n):
         for j in range(i + 1, n):
             i_class = np.where(Z[i, :] == 1)[0][0]
             j_class = np.where(Z[j, :] == 1)[0][0]
             X[i, j] = np.random.binomial(1, pi[i_class, j_class])
             X[j, i] = X[i, j]
+
     return X, Z
 
 
-def random_graph(n=None, Q=None, occupation=0.5):
+def random_graph(
+    n: Optional[int] = None, Q: Optional[int] = None, occupation: float = 0.5
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generates random SBM parameters and graph.
+
+    Parameters
+    ----------
+    n : int, optional
+        Number of nodes in the graph. If not provided, a random value between 2 and 100 will be used.
+    Q : int, optional
+        Number of classes. If not provided, a random value between 2 and n/10 will be used.
+    occupation : float, optional
+        Scaling factor for the connection probabilities. By default 0.5.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+        A tuple containing the adjacency matrix X, the node assignments matrix Z, class membership probabilities alpha, and connection probabilities matrix pi.
+    """
     if n is None:
         n = np.random.randint(2, 100)
     if Q is None:
